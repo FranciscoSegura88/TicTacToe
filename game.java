07535b0515a8f8ad385a601148737eb621a5f069
xcesarg <126008794+xcesarg@users.noreply.github.com>
2023-10-11 08:57:36 -0600

    Add files via upload

--- game.java
+++ game.java
@@ -3,6 +3,12 @@ import java.awt.event.*;
 
 public class game extends Frame implements MouseListener{
 //DECLARE
+MenuBar mb;
+Menu m1, m2, m3;
+MenuItem mi1, mi2, mi3, mi4;
+
+
+
 
     game(){
         //FRAME ATRIBUTES
@@ -10,8 +16,27 @@ public class game extends Frame implements MouseListener{
         setSize(600,400);
         setVisible(true);
         addMouseListener(this);
+         mb = new MenuBar ();
+         m1 = new Menu("Nuevo Juego");
+         m2 = new Menu("Opciones");
+         m3 = new Menu("Ayuda");
+         mi1 = new MenuItem("");
+         mi2 = new MenuItem("");
+         mi3 = new MenuItem("");
 
         //INITIALIZE
+
+         this.addWindowListener(new WindowAdapter(){          //closing window event
+          public void windowClosing(WindowEvent windowEvent){
+           System.exit(0);
+          }      
+         });
+   
+     // ADD 
+      setMenuBar(mb);
+        mb.add(m1);
+        mb.add(m2);
+        mb.add(m3);
         
     }
 
